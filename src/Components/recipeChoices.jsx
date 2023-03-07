f54c37018a9b401f6fa7535e7badbf28446e0263
Wilson Yu <wilsonbinaryfisher@gmail.com>
2023-03-07 17:34:48 -0500

    first commit

---
+++ src/Components/recipeChoices.jsx
@@ -0,0 +1,21 @@
+import React, {Component, useEffect, useState} from "react";
+
+const RecipeChoices = ({handleChange, label, choices, checked}) =>{
+
+    return (
+        <div className="radio-buttons">
+            {choices && choices.map((choice) =>(
+                <li key={choice}>
+                    <input id={choice} value={choice} name={label} type='radio' onChange={handleChange} checked={checked == choice} >
+                    </input>
+                    {choice}
+                </li>
+            ))
+            }
+
+        </div>
+    )
+}
+
+
+export default RecipeChoices;
